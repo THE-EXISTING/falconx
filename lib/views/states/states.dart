f1e1f5930b3e14bae01ea971bdba8d14d10e84b1
Nonthawit Doungsodsri <nonthawit.kub@gmail.com>
2023-07-05 16:03:59 +0700

    [+] update

---
+++ lib/views/states/states.dart
@@ -0,0 +1,7 @@
+// Created by Nonthawit on 5/7/2023 AD Lead Flutter at NEXTZY and EXISTING
+export 'application_state.dart';
+export 'bloc_state.dart';
+export 'state.dart';
+export 'widget_model_state.dart';
+
+ 
--- lib/views/views.dart
+++ lib/views/views.dart
@@ -1,5 +1,3 @@
-export 'package:falconx/views/state/state.dart';
-export 'package:falconx/views/state/widget_model_state.dart';
-export 'package:falconx/views/state/application_state.dart';
+export 'package:falconx/views/states/state.dart';
 export 'package:falconx/views/widget_size.dart';
 
